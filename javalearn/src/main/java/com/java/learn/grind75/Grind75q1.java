530822059585ceae736729daeaa027ab2089fa00
Arpit Singh <arpit.sing@walmart.com>
2025-05-05 01:34:01 +0530

    grind 75

--- javalearn/src/main/java/com/java/learn/grind75/Grind75q1.java
+++ javalearn/src/main/java/com/java/learn/grind75/Grind75q1.java
@@ -6,8 +6,9 @@ public class Grind75q1 {
 
     public static void main(String[] args) {
         Grind75q1 gr = new Grind75q1();
-        String s = "A man, a plan, a canal: Panama";
-        System.out.println(gr.isPalindrome(s));
+//        String s = "A man, a plan, a canal: Panama";
+        String s = "abccccdd";
+        System.out.println(gr.longestPalindrome(s));
     }
 
     //https://leetcode.com/problems/two-sum/
@@ -69,6 +70,107 @@ public class Grind75q1 {
         }
 
     }
+
+
+    public boolean hasCycle(ListNode head) {
+
+        if (head == null) return false; // Fix: empty list can't have a cycle
+
+        ListNode slow = head;
+        ListNode fast = head.next;
+
+        while (fast != null && fast.next != null) {
+            if (slow == fast) return true;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return false;
+    }
+
+
+    private boolean isBadVersion(int version) {
+        return false;
+    }
+
+    public int firstBadVersion(int n) {
+        int left = 0, right = n;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (isBadVersion(mid)) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+
+
+    public boolean canConstruct(String ransomNote, String magazine) {
+
+        int count[] = new int[26];
+
+        for (int i = 0; i < ransomNote.length(); i++) {
+            count[ransomNote.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < magazine.length(); i++) {
+            if (count[magazine.charAt(i) - 'a'] > 0) {
+                count[magazine.charAt(i) - 'a']--;
+            }
+        }
+
+        for (int i = 0; i < 26; i++) {
+            if (count[i] > 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+
+    public int climbStairs(int n) {
+        int dp[] = new int[n + 1];
+        return dpClimbStairs(n, dp);
+    }
+
+    private int dpClimbStairs(int n, int[] dp) {
+        if (n < 0) return 0;
+        if (n == 0) {
+            dp[0] = 1;
+            return 1;
+        }
+        if (dp[n] != 0) return dp[n];
+        int num1 = dpClimbStairs(n - 1, dp);
+        int num2 = dpClimbStairs(n - 2, dp);
+        dp[n] = num1 + num2;
+        return num1 + num2;
+    }
+
+
+    public int longestPalindrome(String s) {
+
+        int twos = 0, ones = 0;
+        int[] hash = new int[256];
+        for (int i = 0; i < s.length(); i++) {
+            hash[s.charAt(i)]++;
+        }
+
+
+        for (int i = 0; i < hash.length; i++) {
+            if (hash[i] != 0) {
+                twos += hash[i] / 2;
+                ones += hash[i] % 2;
+            }
+        }
+
+        System.out.println(twos + ":::" + ones);
+        return (2 * twos) + (ones >= 1 ? 1 : 0);
+
+    }
+
 
     private class ListNode {
         int val;
@@ -216,6 +318,26 @@ public class Grind75q1 {
 
     }
 
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+
+        if (root == null)
+            return null;
+
+        int curr = root.val;
+
+        if (curr < p.val && curr < q.val) {
+            return lowestCommonAncestor(root.right, p, q); // go right
+        }
+        if (curr > p.val && curr > q.val) {
+            return lowestCommonAncestor(root.left, p, q); // go left
+        }
+
+        // If one value is on the left and the other is on the right, root is LCA
+        return root;
+    }
+
+
     public TreeNode invertTreeDFC(TreeNode root) {
 
         if (root == null) return null;
@@ -228,6 +350,29 @@ public class Grind75q1 {
         invertTreeDFC(root.right);
 
         return root;
+
+    }
+
+
+    public boolean isBalanced(TreeNode root) {
+
+        return dfsHeight(root) != -1;
+
+    }
+
+    private int dfsHeight(TreeNode root) {
+
+
+        if (root == null) return 0;
+
+        int left = dfsHeight(root.left);
+        if (left == -1) return -1;
+        int right = dfsHeight(root.right);
+        if (right == -1) return -1;
+        if (Math.abs(left - right) > 1) {
+            return -1;
+        }
+        return Math.max(left, right) + 1;
 
     }
 
@@ -252,10 +397,4 @@ public class Grind75q1 {
     }
 
 }
-
-
-
-
-
-
 
