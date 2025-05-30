3e3de1eb29b27af76157a9fc79658a722b00ed7b
Arpit Singh <arpit.sing@walmart.com>
2025-05-30 17:12:43 +0530

    DAG

--- javalearn/src/main/java/com/java/learn/dsa/lectures/graphs/GraphLec02.java
+++ javalearn/src/main/java/com/java/learn/dsa/lectures/graphs/GraphLec02.java
@@ -1,4 +1,28 @@
 package com.java.learn.dsa.lectures.graphs;
 
+import java.util.HashMap;
+
 public class GraphLec02 {
+
+    public Node cloneGraph(Node node) {
+
+        return cloneGraphHelper(node, new HashMap<Node, Node>());
+
+    }
+
+    private Node cloneGraphHelper(Node node, HashMap<Node, Node> map) {
+
+        if (node == null) return null;
+        if (map.containsKey(node)) return map.get(node);
+
+        Node copy = new Node(node.val);
+        map.put(node, copy);
+        for (Node neighbor : node.neighbors) {
+            copy.neighbors.add(cloneGraphHelper(neighbor, map));
+        }
+
+        return copy;
+    }
+
+
 }
